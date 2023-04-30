f8351f9f0a5b469dfa310b59a69bbdfb553b934e
luka zagorc <zagorcluka1@gmail.com>
2023-04-30 17:21:37 +0200

    update

---
+++ SeminarskaPRO1/fallingmugs.py
@@ -0,0 +1,19 @@
+import math
+
+
+def skodelica():
+    "Preveri če dano število, možna višina skodelice"
+    D = int(input())                                # a**2 - b**2 = D  =>  (a-b)(a+b) = D
+    for a_minus_b in range(1, int(math.sqrt(D)+1)): # gremo skozi možne faktorje števila D od 1 do korena št. (vključno)
+        
+        if D % a_minus_b == 0:                  #preverimo če lahko delimo s faktorjem št. D 
+            a_plus_b = D // a_minus_b           #izračunamo (a+b)
+            if (a_plus_b + a_minus_b) % 2 == 0: #preverimo če sta oba soda ali liha
+                a = (a_plus_b + a_minus_b) // 2 #izračunamo a
+                b = (a_plus_b - a_minus_b) // 2 #izračunamo b
+               
+                return f"{b} {a}"
+    return "impossible"                         #če nismo najdli nobene možne kombinacije izpišemo "impossible"
+
+print(skodelica())
+    
---
+++ SeminarskaPRO1/inversefactorial.py
@@ -0,0 +1,27 @@
+import math
+
+n = input()
+def fakulteta_n(n):
+    "Od razultata n fakultete izračuna število n"
+    n_stevke = len(n)
+    if n_stevke < 2:      #če je število malo lahko kar "na dolgo"  
+        x = 1             #izračunamo n fakultete in primerjamo 
+        N = 1             #razultate z našim n-jem(2.razlog način z logaritmi deluje samo za n>3)
+        while x < int(n):   
+            N += 1
+            x *= N
+        return N
+    else:                 #ko je število večje pa uporabimo lastnost logaritmov in sicer
+        N = 0             #log(a*b) = log(a) + log(b). Ker je hitreje seštevati logaritme
+        stevke = 1        #kot pa računati n!. Uporabimo logaritem z osnovo 10, od koder 
+                          #pridobimo število števk števila(če zaokrožimo navzgor).
+        while stevke < n_stevke:  #ko prekoračimo število števk danega števila 
+            N += 1                #vrnemo iskano število N od N!
+            stevke += math.log(N,10)
+            
+        return N
+        
+
+print(fakulteta_n(n))
+
+#pomoč - https://www.geeksforgeeks.org/count-digits-in-a-factorial-using-logarithm/
